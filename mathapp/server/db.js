7a6f4f44f686c459be119d61fddb29f9e2ac45f8
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2023-02-02 20:16:36 +0700

    edit db

--- mathapp/server/db.js
+++ mathapp/server/db.js
@@ -1,24 +1,38 @@
-const mysql = require('mysql')
-const db = mysql.createConnection({
-    host:"localhost",
+// const mysql = require('mysql')
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user: "root",
+//     password:"",
+//     database:"mathapp"
+// })
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
     user: "root",
-    password:"",
+    password: "",
     database:"mathapp"
-})
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
 //check error
-db.connect(function(err) {
-    if(err) {
-        return console.error('error' + err.message);
-    }
-    console.log('Connect to mysql')
-    sql="insert into exercise (img,mark) values('next.png','มากกว่า')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("insert complete");
-    }); 
+// db.connect(function(err) {
+//     if(err) {
+//         return console.error('error' + err.message);
+//     }
+//     console.log('Connect to mysql')
+//     sql="insert into exercise (img,mark) values('next.png','มากกว่า')";
+//     con.query(sql, function (err, result) {
+//         if (err) throw err;
+//         console.log("insert complete"); 
+//     }); 
 
-})
+// })
 
 
-module.exports = db;
+module.exports = mysql;
 
