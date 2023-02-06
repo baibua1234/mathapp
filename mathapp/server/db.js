5c1a35ddde33651204f086e5b6070f6cf7c6da24
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2023-02-06 20:30:14 +0700

    add

--- mathapp/server/db.js
+++ mathapp/server/db.js
@@ -16,15 +16,9 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Table created");
+  if (err) throw err;
+  console.log("Connected!");
 });
-
-})
 
 
 module.exports = mysql;
